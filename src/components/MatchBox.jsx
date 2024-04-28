2815e4072685c8c5ba0ff09a8cdb52af36fc7fd6
Mojin2 <aiduriaaa@gmail.com>
2024-04-28 22:34:40 +0900

    [API] Match Details

--- src/components/MatchBox.jsx
+++ src/components/MatchBox.jsx
@@ -1,45 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function MatchBox(props) {
-  const { matchid } = props;
+  const { match } = props;
   const [clicked, setClicked] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [info, setInfo] = useState({});
+
   const handleClick = () => {
     setClicked(!clicked);
-    fetchMatchDetails(matchid);
   };
   const APIKEY = process.env.REACT_APP_API_KEY;
-  async function fetchMatchDetails(matchid) {
-    try {
-      const response = await axios.get(
-        `https://open.api.nexon.com/fconline/v1/match-detail?matchid=${matchid}`,
-        {
-          headers: {
-            "x-nxopen-api-key": APIKEY,
-          },
-        }
-      );
-      setInfo(response.data);
-      console.log(info);
-    } catch (error) {
-      console.error(error);
-      setIsError(true);
-    }
-  }
+
   return (
     <div>
       <div className="bg-red-500 w-full h-[100px] flex justify-between">
-        {matchid}
+        {match.matchId}
         <button className="bg-yellow-500" onClick={handleClick}>
           열기
         </button>
       </div>
       <div
-        className={`bg-blue-500 w-full h-[300px] ${clicked ? "" : "hidden"}`}
+        className={`bg-blue-500 w-full h-[400px] flex justify-between px-10 ${
+          clicked ? "" : "hidden"
+        }`}
       >
-        {info.matchInfo}
+        {/* {Array.isArray(info) ? (
+          info.map((inf, index) => (
+            <div key={index}>{inf.matchDetail.matchResult}</div>
+          ))
+        ) : (
+          <div>Loading...</div>
+        )} */}
       </div>
     </div>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,32 +4,69 @@ import { useNavigate, useParams } from "react-router-dom";
 import MatchBox from "./MatchBox";
 
 export default function Search() {
-  const [matchDataList, setMatchDataList] = useState([]);
   const [isError, setIsError] = useState(false);
   const APIKEY = process.env.REACT_APP_API_KEY;
   const params = useParams();
   const navigate = useNavigate();
+  const [matchList, setMatchList] = useState([]);
 
   // matchdata fetch
-  async function fetchMatchDataList(ouid, matchtype, offset, limit) {
+  async function fetchMatchList(ouid, matchtype, offset, limit) {
+    const storedMatchList = localStorage.getItem(ouid);
+    // 로컬 스토리지에 저장되어 있는 경우
+    if (storedMatchList) {
+      const parsedData = JSON.parse(storedMatchList);
+      setMatchList(parsedData);
+      console.log("cached match list");
+      return;
+    }
+
+    // 로컬 스토리지에 저장되어 있지 않는 경우 API호출
     try {
-      const response = await axios.get(
-        `https://open.api.nexon.com/fconline/v1/user/match?ouid=${ouid}&matchtype=${matchtype}&offset=${offset}&limit=${limit}`,
-        {
-          headers: {
-            "x-nxopen-api-key": APIKEY,
-          },
-        }
-      );
-      setMatchDataList(response.data);
+      await axios
+        .get(
+          `https://open.api.nexon.com/fconline/v1/user/match?ouid=${ouid}&matchtype=${matchtype}&offset=${offset}&limit=${limit}`,
+          {
+            headers: {
+              "x-nxopen-api-key": APIKEY,
+            },
+          }
+        )
+        .then((response) => {
+          const matchIdList = response.data;
+          console.log(matchIdList);
+          const fetchMatchDetails = matchIdList.map(
+            async (matchId) =>
+              await axios
+                .get(
+                  `https://open.api.nexon.com/fconline/v1/match-detail?matchid=${matchId}`,
+                  {
+                    headers: {
+                      "x-nxopen-api-key": APIKEY,
+                    },
+                  }
+                )
+                .then((response) => response.data)
+          );
+          Promise.all(fetchMatchDetails)
+            .then((matchDetails) => {
+              setMatchList(matchDetails);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        });
+      localStorage.setItem(ouid, JSON.stringify(matchList));
+      console.log("save to localstorage");
     } catch (error) {
       console.error(error);
-      setIsError(true);
     }
   }
+
   useEffect(() => {
-    // fetchMatchDataList(params.ouid, 40, 0, 100);
-  });
+    fetchMatchList(params.ouid, 40, 0, 100);
+  }, []);
+
   return (
     <div>
       {isError ? (
@@ -37,9 +74,9 @@ export default function Search() {
       ) : (
         <div>
           <div>{params.ouid}의 페이지</div>
-          {/* {matchDataList.map((matchid, index) => (
-            <MatchBox matchid={matchid} key={index} />
-          ))} */}
+          {matchList.map((match, index) => (
+            <MatchBox match={match} key={index} />
+          ))}
         </div>
       )}
     </div>
